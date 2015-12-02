270357354141f0af971fdb83d4d3e197fbc1773f
Barret J. Nobel <bear.nobel@gmail.com>
2015-12-01 21:10:18 -0500

    created LoginInterface Branch

--- src/LoginInterface.java
+++ src/LoginInterface.java
@@ -6,45 +6,45 @@ import java.util.Scanner;
 
 public class LoginInterface
 {
-    public static void main(String[] args)
-    {
-        String username; //............................. variables
-        String password;
-        boolean loginSuccess = false;
-        Scanner console = new Scanner( System.in );
-
-        do
-        {
-            try
-            {
-                System.out.print( generateHeader("staff") ); //............................... print header
-                username = console.next(); //................................................. capture username
-                System.out.print( "Password: " ); //.......................................... prompt for the password
-                password = console.next();
-            }
-            catch (Exception e)
-            {
-                System.out.println( "\nInvalid Username or password, try again\n\n" );
-                Object temp = console.next(); //.............................................. captures the return character
-            }
-
-            // NEED TO CREATE ACCOUNT OBJECT AND VERIFY CREDENTIALS
-            // IF ITS VALID SET LOGIN SUCCESS TO TRUE
-
-        } while( !loginSuccess );
-
-    }
-
-    // Method to generate the header for the login system
-    // takes 1 String argument
-    // determines if it will display Staff or User Login
-    private static String generateHeader( String userOrStaff )
-    {
-        String returnString = String.format( "%s%n%s%n%s%n%n", "*********************", "* Hotel Reservation *", "*********************" );
-
-        returnString += userOrStaff.equals("user") ? "User Login: " : "Staff Login: "; //...................................................... add more to the return string
-
-        return returnString;
-    }
+//    public static void main(String[] args)
+//    {
+//        String username; //............................. variables
+//        String password;
+//        boolean loginSuccess = false;
+//        Scanner console = new Scanner( System.in );
+//
+//        do
+//        {
+//            try
+//            {
+//                System.out.print( generateHeader("staff") ); //............................... print header
+//                username = console.next(); //................................................. capture username
+//                System.out.print( "Password: " ); //.......................................... prompt for the password
+//                password = console.next();
+//            }
+//            catch (Exception e)
+//            {
+//                System.out.println( "\nInvalid Username or password, try again\n\n" );
+//                Object temp = console.next(); //.............................................. captures the return character
+//            }
+//
+//            // NEED TO CREATE ACCOUNT OBJECT AND VERIFY CREDENTIALS
+//            // IF ITS VALID SET LOGIN SUCCESS TO TRUE
+//
+//        } while( !loginSuccess );
+//
+//    }
+//
+//    // Method to generate the header for the login system
+//    // takes 1 String argument
+//    // determines if it will display Staff or User Login
+//    private static String generateHeader( String userOrStaff )
+//    {
+//        String returnString = String.format( "%s%n%s%n%s%n%n", "*********************", "* Hotel Reservation *", "*********************" );
+//
+//        returnString += userOrStaff.equals("user") ? "User Login: " : "Staff Login: "; //...................................................... add more to the return string
+//
+//        return returnString;
+//    }
 
 }
