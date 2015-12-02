7456da4d22a2ef739bddb47099e989eba18b02df
Barret J. Nobel <bear.nobel@gmail.com>
2015-12-01 21:41:47 -0500

    wrote newAccountMenu method adjusted some of the prompt's

--- src/LoginInterface.java
+++ src/LoginInterface.java
@@ -1,13 +1,56 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 /**
  * Created by Barret J. Nobel on 12/1/2015.
  */
 
-import java.util.Scanner;
-
 public class LoginInterface
 {
-//    public static void main(String[] args)
-//    {
+    public static String initialMenu() {
+        String returnString = generateHeader( "Hotel Reservation" );
+        returnString += "Please choose from the following:\n";
+        returnString += "1. Login\n";
+        returnString += "2. Create a new Account\n>:";
+
+        return returnString;
+    }
+
+    public static String loginPrompt( String userOrStaff )
+    {
+        String returnString = generateHeader( "Login" );
+        returnString += userOrStaff.equals("user") ? "User Login: " : "Staff Login: ";
+
+        return returnString;
+    }
+
+    public static String passwordPrompt()
+    {
+        return "Password: ";
+    }
+
+    public static String newAccountMenu()
+    {
+        String returnString = generateHeader( "New Account" );
+        returnString += "Choose from the menu:\n";
+        returnString += "1.Staff Account\n";
+        returnString += "2.User Account\n>:";
+
+        return returnString;
+    }
+
+    private static String generateHeader( String titleIn )
+    {
+        String stars = "*********************";
+        String title = String.format( "*%-19s*", titleIn );
+        return String.format("%s%n%s%n%s%n%n", stars, title, stars);
+    }
+
+    public static void main(String[] args)
+    {
+
+        System.out.print(loginPrompt("user") );
+        System.out.println( passwordPrompt());
+        System.out.print( newAccountMenu());
 //        String username; //............................. variables
 //        String password;
 //        boolean loginSuccess = false;
@@ -32,9 +75,9 @@ public class LoginInterface
 //            // IF ITS VALID SET LOGIN SUCCESS TO TRUE
 //
 //        } while( !loginSuccess );
-//
-//    }
-//
+
+    }
+
 //    // Method to generate the header for the login system
 //    // takes 1 String argument
 //    // determines if it will display Staff or User Login
